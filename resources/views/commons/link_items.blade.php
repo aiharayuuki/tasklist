41a8069c40c25e4d36daea7ed5d2278a642f19da
相原優樹 <yuki.aihara.151@gmail.com>
2025-07-18 22:28:24 +0900

    navbarとlink_itemsの追加・修正

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,3 @@
+{{-- resources/views/commons/link_items.blade.php --}}
+<li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+<li><a href="{{ route('tasks.index') }}">Tasks</a></li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>Microposts</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        @vite('resources/css/app.css')
-    </head>
 
-    <body>
+<head>
+    <meta charset="utf-8">
+    <title>Microposts</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.6.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com/3.4.1"></script>
+</head>
 
-        {{-- ナビゲーションバー --}}
-        @include('commons.navbar')
+<body>
 
-        <div class="container mx-auto">
-            {{-- エラーメッセージ --}}
-            @include('commons.error_messages')
+    {{-- ナビゲーションバー --}}
+    @include('commons.navbar')
 
-            @yield('content')
-        </div>
+    <div class="container mx-auto">
+        {{-- エラーメッセージ --}}
+        @include('commons.error_messages')
 
-    </body>
+        @yield('content')
+    </div>
+
+</body>
+
 </html>
